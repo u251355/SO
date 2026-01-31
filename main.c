c1d54f0f83d6f862f33ddf443c050b4a1acb160b
u251355 <elena.sandin01@estudiant.upf.edu>
2026-01-31 19:51:50 +0100

    cambios

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ long sumBinary(int fd, int bufferSize) { //function to sum all integers from a b
 
     bufferSize -= bufferSize % sizeof(int); // allocate memory to store data read from file
     int *buffer = malloc(bufferSize);
-    if (!buffer) {  // if malloc failed exit
+    if (!buffer) {  // if malloc failed exits
         perror("malloc");
         exit(1);
     }
@@ -18,8 +18,7 @@ long sumBinary(int fd, int bufferSize) { //function to sum all integers from a b
     int bytesRead; // variable to store how many bytes read from file
 
     while ((bytesRead = read(fd, buffer, bufferSize)) > 0) { // read from the file while there is still data
-
-        int count = bytesRead / sizeof(int);// how many integers were actually read
+        int count = bytesRead / sizeof(int);// how many integers are read
         for (int i = 0; i < count; i++) { // add each integer in the buffer to the sum
             sum += buffer[i];
         }
@@ -36,7 +35,6 @@ long sumText(int fd, int bufferSize) {
 
     CircularBuffer cb;
     buffer_init(&cb, bufferSize);
-
     char *temp = malloc(bufferSize);
     if (!temp) {
         perror("malloc");
@@ -65,7 +63,6 @@ long sumText(int fd, int bufferSize) {
 
         /* ---- Extract complete numbers ---- */
         int elemSize = buffer_size_next_element(&cb, ',', reachedEOF);
-
         while (elemSize != -1) {
 
             char numberStr[elemSize + 1];
@@ -75,21 +72,17 @@ long sumText(int fd, int bufferSize) {
             }
 
             numberStr[elemSize] = '\0';
-
             // Remove comma if present
             if (numberStr[elemSize - 1] == ',')
                 numberStr[elemSize - 1] = '\0';
-
             int num = atoi(numberStr);
             sum += num;
-
             elemSize = buffer_size_next_element(&cb, ',', reachedEOF);
         }
     }
 
     free(temp);
     buffer_deallocate(&cb);
-
     return sum;
 }
 
@@ -102,14 +95,13 @@ int main(int argc, char *argv[]) {
     }
 
     char *mode = argv[1]; // the first argument mode (binary or text)
-    char *filePath = argv[2]; // the second argument: path to the input file
-    int bufferSize = atoi(argv[3]); //  the third argument: buffer size 
+    char *filePath = argv[2]; // the second argument path to the input file
+    int bufferSize = atoi(argv[3]); //  the third argument buffer size 
     int fd = open(filePath, O_RDONLY); // open the file in read only mode
-    if (fd < 0) {  //if the file can't not be opened exits
+    if (fd < 0) {  //if the file can't be opened exits
         perror("open");
         return 1;
     }
-
     long result; // variable to store the final result
     if (strcmp(mode, "binary") == 0) {  // if binary mode
         result = sumBinary(fd, bufferSize);
