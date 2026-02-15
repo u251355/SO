25aa665ddd79e9a98a1f4b57944bcf305a97682c
SaraViu <sara.viu01@estudiant.upf.edu>
2026-02-15 12:54:06 +0100

    comments

--- Practice 2/code/main.c
+++ Practice 2/code/main.c
@@ -85,54 +85,56 @@ int main() {
                // parent does not wait 
                  }
         // PIPED execution
-        else if (strcmp(line, "PIPED") == 0 || strcmp(line, "PIPE") == 0) {
-            char line1[BUFFER_SIZE];
-            char line2[BUFFER_SIZE];
+        else if (strcmp(line, "PIPED") == 0 || strcmp(line, "PIPE") == 0) { // we check if the user has introduced the PIPE or PIPED execution mode
+            char line1[BUFFER_SIZE]; // it stores the first command
+            char line2[BUFFER_SIZE]; // it stores the second command
+            // read two lines from the input using the circular buffer
             read_line(STDIN_FILENO, &cb, line1, BUFFER_SIZE, &reachedEOF);
             read_line(STDIN_FILENO, &cb, line2, BUFFER_SIZE, &reachedEOF);
-            int pipefd[2];
-            if (pipe(pipefd) < 0) {
+            int pipefd[2]; // it creates a new pipe 
+            if (pipe(pipefd) < 0) { // error control
                 perror("pipe");
                 exit(1);
             }
-            pid_t pid1 = fork();
-            if (pid1 < 0) {
+            pid_t pid1 = fork(); // it creates a child
+            if (pid1 < 0) { // error control
                 perror("fork");
                 exit(1);
             }
-            if (pid1 == 0) {
-                // First process writes to pipe
-                dup2(pipefd[1], STDOUT_FILENO);
+            if (pid1 == 0) { // code for the first child
+                dup2(pipefd[1], STDOUT_FILENO); // it redirects the output to the pipe
+                // it closes both ends of the pipe
                 close(pipefd[0]);
                 close(pipefd[1]);
-                char **argv1 = split_command(line1);
-                execvp(argv1[0], argv1);
-                perror("execvp");
+                char **argv1 = split_command(line1); // convert the command to argv format 
+                execvp(argv1[0], argv1); // replace the child process with the program of the first command
+                perror("execvp"); // if it fails (error control)
                 exit(1);
             }
-            pid_t pid2 = fork();
-            if (pid2 < 0) {
+            pid_t pid2 = fork(); // it creates the second child 
+            if (pid2 < 0) { // error control
                 perror("fork");
                 exit(1);
             }
-            if (pid2 == 0) {
-                // Second process reads from pipe
-                dup2(pipefd[0], STDIN_FILENO);
+            if (pid2 == 0) { // code for the second child
+                dup2(pipefd[0], STDIN_FILENO); // redirects the input to the read end of the pipe
+                // closes the descriptors that are not necessary
                 close(pipefd[1]);
                 close(pipefd[0]);
-                char **argv2 = split_command(line2);
+                char **argv2 = split_command(line2); // executes the second command
                 execvp(argv2[0], argv2);
-                perror("execvp");
+                perror("execvp"); // error control
                 exit(1);
             }
-
+            // closes both ends
             close(pipefd[0]);
             close(pipefd[1]);
+            // waits until both children are finished
             waitpid(pid1, NULL, 0);
             waitpid(pid2, NULL, 0);
         }
     }
 
-    buffer_deallocate(&cb);
+    buffer_deallocate(&cb); // frees the memory of the circcular buffer
     return 0;
 }
