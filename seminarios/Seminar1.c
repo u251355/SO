f48678e4f2dfc21a6146e737a49c1a1e7105ccb9
u251355 <elena.sandin01@estudiant.upf.edu>
2026-02-18 19:32:35 +0100

    s

--- seminarios/Seminar1.c
+++ seminarios/Seminar1.c
@@ -4,14 +4,30 @@
 int main(){
 int count =0;
 char c; 
-while (read(0, &c, 1) > 0) {   //mientras lea, std input
+while (read(0, &c, 1) > 0) {   //mientras lea, std input fd=0
         if (c == 'a') //si la letra coincide
         count++; //suma
     }
     char buffer[20];
     int len = sprintf(buffer, "%d\n", count); //convierte a numero y lo guarda en el buffer
-    write(1, buffer, len); //1 is the standard output
+    write(1, buffer, len); //1 is the standard output fd=1
     return 0;
 }
+ 
+//EX2
+int main2()´{
+int fd1 = open (text,O_WRONLY|O_CREATE, 0644);//siempre es asi
+int fd2 = open (binary,O_WRONLY|O_CREATE, 0644);
+char buffer [70];
+   for(int i=0;i<100;i++) { //100 cosas
+        int n = rand(); //crear numeros random
+        int len = sprintf(buffer,"%d,",n); //convierte a numero
+        write(fd1, buffer, len);//donde, desde donde, que
+        write(fd_bin, &n, sizeof(n));
+    }
+    close(fd1);
+    close(fd2);
+}
+
 
 
