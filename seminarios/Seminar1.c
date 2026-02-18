4651ab96ef4d95541759fed5c946740f99dfea2e
u251355 <elena.sandin01@estudiant.upf.edu>
2026-02-18 19:37:52 +0100

    f

--- seminarios/Seminar1.c
+++ seminarios/Seminar1.c
@@ -9,19 +9,19 @@ while (read(0, &c, 1) > 0) {   //mientras lea, std input fd=0
         count++; //suma
     }
     char buffer[20];
-    int len = sprintf(buffer, "%d\n", count); //convierte a numero y lo guarda en el buffer
+    int len = sprintf(buffer, "%d\n", count); // lo guarda en el buffer, en formato d el counter(para escribir en strings como buffer)
     write(1, buffer, len); //1 is the standard output fd=1
     return 0;
 }
  
 //EX2
 int main2()´{
-int fd1 = open (text,O_WRONLY|O_CREATE, 0644);//siempre es asi
-int fd2 = open (binary,O_WRONLY|O_CREATE, 0644);
+int fd1 = open (text.txt,O_WRONLY|O_CREATE, 0644);//siempre es asi
+int fd2 = open (binary.dat,O_WRONLY|O_CREATE, 0644);
 char buffer [70];
    for(int i=0;i<100;i++) { //100 cosas
         int n = rand(); //crear numeros random
-        int len = sprintf(buffer,"%d,",n); //convierte a numero
+        int len = sprintf(buffer,"%d,",n); //guardamos en el buffer
         write(fd1, buffer, len);//donde, desde donde, que
         write(fd_bin, &n, sizeof(n));
     }
