58d3b83d10e39eaccdc3e07c6149ffeedf18f33c
u251355 <elena.sandin01@estudiant.upf.edu>
2026-02-25 13:58:44 +0100

    ex2

--- seminarios/Seminar4.c
+++ seminarios/Seminar4.c
@@ -26,10 +26,32 @@ int main(int argc, char *argv[])// el que sendea
     _exit(0);
 }
 
-
-
-
-
-
-
+//Ex2
+volatile sig_atomic_t step = 1;
+void handler(int sig)//dice que vaya suemando de mas en mas
+{
+    step++;   
+}
+int main()
+{
+    signal(SIGINT, handler); //señal se llama tal y se mandara al handler
+    int i = 0;
+    char c;
+    while (i <= 500)
+    {
+        if (i >= 100) {
+            c = (i / 100) + '0';
+            write(1, &c, 1);
+        }
+        if (i >= 10) {
+            c = ((i / 10) % 10) + '0';
+            write(1, &c, 1);
+        }
+        c = (i % 10) + '0';
+        write(1, &c, 1);
+        write(1, "\n", 1);
+        usleep(100000);  
+        i += step;
+    }
+    _exit(0);
 }
