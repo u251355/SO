2148a9675b9703a5e68db16fa5036d25d301e2f5
u251355 <elena.sandin01@estudiant.upf.edu>
2026-03-08 13:28:05 +0100

    ex

--- seminarios/Examen1b.c
+++ seminarios/Examen1b.c
@@ -10,24 +10,44 @@ unsigned char is_printable(char c) {  // TE LO DAN
  }
 }
 char buffer[100];
-int main(int argc, char*argv){
+int main(int argc, char*argv[]){
   int  fd1[2]; //siempre las pipes antes que los hijos
   int fd2[2];
+  pipe(fd1);
+  pipe(fd2);
   pid_t pid = fork(); //hijo 1
-  while(int read(buffer,argv[], 1 )>0){//mientras haya algo que leer
+
+  if(pid!=0){
     close(fd1[0]);
-    write(fs1[1], buffer, 1)//s elo escribe al hijo
+    close(fd2[0]);
+    close(fd2[1]);
+  int ar= open(argv[1], O_RDONLY,0644);//abrimos el archivo
+  int bytes;
+  while( bytes= read(ar,buffer, 1 )>0){//mientras haya algo que leer
+     for (bytes i = 0; i < bytes; i++) {
+    write(fd1[1], buffer, 1)//se lo escribe al hijo
   }
-if (pid ==o){ //el hijo crea otro hijo
-    pid_t pid2= fork();
-    
+  wait(NULL);
+  }
+close(ar);
+close(fd1[1]);
+if (pid ==0){ //el hijo crea otro hijo
+    pid_t pid2= fork();//hijo 2
+    close(fd1[1]);
+    char c;
+    read( fd1[0], &c, 1);
+    unsigned char filtrada = is_printable(c);
+    close(fd2[0]);
+    write(fd2[1], &filtrada,1);
+
+if (pid2==0){
+    close(fd2[1]);
+   while( read(fd2[0],&buffer, 1)>0){
+    write(1,&buffer, 1)
 }
-
-
-
-
-
-
-
+}
+wait(NULL);
+}
+return 0
 
 }
