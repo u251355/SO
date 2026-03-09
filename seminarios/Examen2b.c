70e6bbd953c44f820eadabdbe676c1c3d6964935
u251355 <elena.sandin01@estudiant.upf.edu>
2026-03-09 18:17:23 +0100

    Ex2b

--- seminarios/Examen2b.c
+++ seminarios/Examen2b.c
@@ -5,23 +5,27 @@
 #include<pthread.h>
 int sum=0;//global pq se necesita tmb en el codigo del thread
 pthread_mutex_t lock;//esto siempre fuera
+sem_t sem;//siempre fuera tambien
 void* sumation(void *arg){ //codigo del thread
     char *file = (char*) arg; //SIEMPRE
    int fd= open(file,O_RDONLY,0644);
    int bytes;
    int c;
+   sem_wait(&sem);//el semaforo hace esperar
    while(bytes=read(fd,&c, sizeof(int))>0){//siempre pq sino no lo lees todo
     pthread_mutex_lock(&lock);
    sum=sum+c;
    pthread_mutex_unlock(&lock);
    }
    close(fd);
+   sem_signal(&sem);//indica que ya esta
    return NULL;
 }
 
 int main(int argc, char* argv[]){
 pthread_t threads[32];
-pthread_mutex_init(&lock,NULL);
+pthread_mutex_init(&lock,NULL);//inicializar todo en el main siempre
+sem_init(&sem,2);
 for(int i=0; i<argc; i++){
     pthread_create(&threads[i],NULL,sumation, argv[i]);//creas los threads
 }
