b79106a274942c5abd1c04760fdfb073899ea249
u251355 <elena.sandin01@estudiant.upf.edu>
2026-03-12 17:21:20 +0100

    g

--- seminarios/Examen4b.c
+++ seminarios/Examen4b.c
@@ -5,6 +5,7 @@
 #include<pthread.h>
 #include<semaphore.h>
 pthread_mutex_t lock;//inicializamos el lock fuera de todo
+
 void* increment(void* arg){
    int i = *(int*)arg;//casteamos
    pthread_mutex_lock(&lock);
@@ -12,9 +13,10 @@ void* increment(void* arg){
    pthread_mutex_unlock(&lock);
    return NULL
 }
+
 int main(int argc, char* argv[]){
         int A[50];
-        int thread[50];
+        int thread[50];// inicializamos thread y mutex
         pthread_mutex_init(&lock, NULL);
         for (int i=0; i<50;i++){
             int *index = malloc(sizeof(int));  // SIEMPRE QUE QUERAMOS PASRELE A OS THREADS ALGO UNICO reservar memoria para un int
